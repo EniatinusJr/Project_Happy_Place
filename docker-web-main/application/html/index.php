46964e20251a2b4e1abda135bf22588f3e826b6f
Maurin Schucan <maurin@schucan.ch>
2021-01-11 13:55:31 +0100

    Added new code

--- docker-web-main/application/html/index.php
+++ docker-web-main/application/html/index.php
@@ -3,34 +3,100 @@
     <head>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/openlayers/openlayers.github.io@master/en/v6.5.0/css/ol.css" type="text/css">
         <style>
-            .map {
-            height: 400px;
+        * {
+            margin: 0;
+            padding: 0;
+        }
+        .map {
+            height: 100vh;
             width: 100%;
-            }
+            position: absolute;
+            top: 0;
+            left: 0;
+            z-index: -9;
+        }
         </style>
         <script src="https://cdn.jsdelivr.net/gh/openlayers/openlayers.github.io@master/en/v6.5.0/build/ol.js"></script>
-        <meta charset="utf-8">
-        <title>OpenLayers example</title>
+        <title>Project Happy Place</title>
     </head>
     <body>
-        <h2>My Map</h2>
         <div id="map" class="map"></div>
-        <?php
-        phpinfo();
-        ?>
         <script type="text/javascript">
-            var map = new ol.Map({
+        var map = new ol.Map({
             target: 'map',
             layers: [
-                new ol.layer.Tile({
-                source: new ol.source.OSM()
+            new ol.layer.Tile({
+                /*
+    ["http://a.tile2.opencyclemap.org/transport/{z}/{x}/{y}.png","http://b.tile2.opencyclemap.org/transport/{z}/{x}/{y}.png","http://c.tile2.opencyclemap.org/transport/{z}/{x}/{y}.png"]
+    ["http://a.tile3.opencyclemap.org/landscape/{z}/{x}/{y}.png","http://b.tile3.opencyclemap.org/landscape/{z}/{x}/{y}.png","http://c.tile3.opencyclemap.org/landscape/{z}/{x}/{y}.png"]
+    ["http://a.tile.openstreetmap.org/{z}/{x}/{y}.png","http://b.tile.openstreetmap.org/{z}/{x}/{y}.png","http://c.tile.openstreetmap.org/{z}/{x}/{y}.png"]
+    ["http://otile1.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png","http://otile2.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png","http://otile3.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png","http://otile4.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png"]
+    ["http://a.tile.stamen.com/watercolor/{z}/{x}/{y}.png","http://b.tile.stamen.com/watercolor/{z}/{x}/{y}.png","http://c.tile.stamen.com/watercolor/{z}/{x}/{y}.png","http://d.tile.stamen.com/watercolor/{z}/{x}/{y}.png"]
+    ["http://a.tile2.opencyclemap.org/transport/{z}/{x}/{y}.png","http://b.tile2.opencyclemap.org/transport/{z}/{x}/{y}.png","http://c.tile2.opencyclemap.org/transport/{z}/{x}/{y}.png"]
+    */
+                source: new ol.source.XYZ({
+                    urls : ["http://a.tile.openstreetmap.org/{z}/{x}/{y}.png","http://b.tile.openstreetmap.org/{z}/{x}/{y}.png","http://c.tile.openstreetmap.org/{z}/{x}/{y}.png"]
                 })
+
+                /*source: new ol.source.OSM()*/
+            }),
+            new ol.layer.Vector({
+                source: new ol.source.Vector({
+                format: new ol.format.GeoJSON(),
+                url: './assets/data/countries.geojson' // GeoCountries file from github
+                })
+            })
             ],
             view: new ol.View({
-                center: ol.proj.fromLonLat([47.35994, 8.52119]),
-                zoom: 10
+            center: ol.proj.fromLonLat([8.5208324, 47.360127]),
+            zoom: 10
             })
-            });
+        });
         </script>
+        <form action="">
+            <input type="text" name="first" placeholder="Firstname">
+            <input type="text" name="last" placeholder="Lastname">
+            <button type="submit" name="submit">submit</button>
+        </form>
+        <?php
+            $servername = "mariadb";
+            $user = "root";
+            $password = "happyplace";
+            $dbname = "happyplace";
+
+            $connection = new mysqli($servername, $user, $password, $dbname);
+
+            if ($connection->connect_error) {
+                die("Connection failed: " . $connection->connect_error);
+            }
+            if (isset($_POST['submit-search'])) {
+                $searchedperson = $_POST['personsearch'];
+                $searchedperson_last = $_POST['personsearch-last'];
+                $sql_full = "SELECT * FROM Lernende WHERE Vorname='" . $searchedperson . "' AND Name='$searchedperson_last';";
+                $result_full = $connection->query($sql_full);
+                $sql_appr = "SELECT Vorname, Name FROM Lernende WHERE Vorname='" . $searchedperson . "' AND Name='$searchedperson_last';";
+                $result_appr = $connection->query($sql_appr);
+                if ($result_full->num_rows > 0) {
+                    $row_full = $result_full->fetch_array(MYSQLI_BOTH);
+                    $row_appr = $result_appr->fetch_array(MYSQLI_BOTH);
+                    $place_id = $row_full[3];
+                    $marker_id = $row_full[4];
+                    $sql_place = "SELECT latitude, longitude FROM Ort WHERE id=" . $OrtId . ";";
+                    $sql_marker = "SELECT color FROM Marker WHERE id=" . $MarkerId . ";";
+                    $result_places = $connection->query($sql_place);
+                    $result_marker = $connection->query($sql_marker);
+                    $row_places = $result_places->fetch_array(MYSQLI_BOTH);
+                    $row_marker = $result_marker->fetch_array(MYSQLI_BOTH);
+                    echo "
+                    <script type='text/javascript'>
+                        add_map_point(" . $row_places[1] . ", " . $row_places[0] . ");
+                    </script>";;
+                } else {
+                    echo "<p id='result-id' class='result'>0 results</p>";
+                }
+            
+            }
+            $connection->close();
+        ?>
     </body>
 </html>
